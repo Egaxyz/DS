e86d107971a344c55fd985bf4140fe3bdb523014
Egaxyz <egagunawannovaldi@gmail.com>
2023-11-19 14:40:00 +0700

    Login beres

--- admin.php
+++ admin.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require "conn.php";
 ?>
 
@@ -9,9 +8,10 @@ require "conn.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
     <style>
-    <?php include "assets/bootstrap.min.css"?>.main {
+    .main {
         height: 100vh;
     }
 
@@ -30,47 +30,49 @@ require "conn.php";
             <form action="" method="post">
                 <div>
                     <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username" name="username">
+                    <input type="text" name="username" class="form-control" id="username">
                 </div>
                 <div>
                     <label for="password">Password</label>
-                    <input type="password" class="form-control" name="password" id="password">
+                    <input type="password" name="password" class="form-control" id="password">
                 </div>
                 <div>
-                    <button class="btn btn-success form-control mt-3" type="submit" name="loginbtn">Login</button>
+                    <button type="submit" class="btn btn-success form-control mt-3" name="loginbtn">Login</button>
                 </div>
             </form>
         </div>
-        <div class="mt-3" style="width: 500px; text-align: center;">
+
+        <div class="mt-3" style="width:500px; text-align:center;">
             <?php
-            if (isset($_POST["loginbtn"])) {
-                $username = htmlspecialchars($_POST["username"]);
-                $password = htmlspecialchars($_POST["password"]);
+        if (isset($_POST["loginbtn"])) {
+            $username = htmlspecialchars_decode($_POST["username"]);
+            $password = htmlspecialchars_decode($_POST["password"]);
 
-                $query = mysqli_query($koneksi, "SELECT*FROM adm WHERE username = '$username'");
-                $countdata = mysqli_num_rows($query);
-                $data = mysqli_fetch_array($query);
-
+            $query = mysqli_query($koneksi, "SELECT*FROM adm WHERE username = '$username'",);
+            $countdata= mysqli_num_rows($query);
+            $data = mysqli_fetch_array($query);
                 if ($countdata > 0) {
-                    if (password_verify($password, $data["password"])) {
-                        echo "benar";
+                    if(isset($password, $data['password'])) {
+                        header('location: index.php');
+                        exit();
                     }
-                  else {
-                    ?>
+                    else{
+                                            ?>
             <div class="alert alert-warning" role="alert">
-                Password Salah!
+                Password Salah
             </div>
             <?php
                     }
-                } else {
+                }
+                else{
                     ?>
             <div class="alert alert-warning" role="alert">
-                Username Salah!
+                Akun tidak tersedia
             </div>
             <?php
                 }
-            }
-            ?>
+        }
+        ?>
         </div>
     </div>
 </body>
